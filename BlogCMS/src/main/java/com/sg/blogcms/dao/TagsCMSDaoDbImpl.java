6d655077bffcfc8acead50e0a2c2c0cb3222d82a
Richard Sevilla <svll.nyc@gmail.com>
2018-06-03 15:32:18 -0400

    wip BJKR-26

--- BlogCMS/src/main/java/com/sg/blogcms/dao/TagsCMSDaoDbImpl.java
+++ BlogCMS/src/main/java/com/sg/blogcms/dao/TagsCMSDaoDbImpl.java
@@ -5,7 +5,11 @@
  */
 package com.sg.blogcms.dao;
 
+import com.sg.blogcms.dto.Tags;
+import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -17,5 +21,57 @@ public class TagsCMSDaoDbImpl implements TagsCMSDao {
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+    
+    //==========================================================================
+    //                                  SQL TAGS
+    //==========================================================================
+    
+    private static final String SQL_INSERT_BLOG
+            = "insert into Tag (tagName, tagDescription)"
+            + "values(?,?)";
+    
+    
+    
+    //==========================================================================
+    //                              METHODS
+    //==========================================================================
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+    public Tags createTag(Tags tag) {
+        jdbcTemplate.update(SQL_INSERT_BLOG,
+                tag.getTagName(),
+                tag.getDescription());
+        int tagID =
+                jdbcTemplate.queryForObject("select LAST_INSERTED_ID()",
+                                                Integer.class);
+        tag.setTagId(tagID);
+        return tag;
+    }
+
+    @Override
+    public void removeTag(int tagID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Tags updateTag(Tags tag) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Tags SelectTag(int tagID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Tags> SelectAllTags() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+       
+    
+    //==========================================================================
+    //                              HELPER METHODS
+    //==========================================================================
     
 }
